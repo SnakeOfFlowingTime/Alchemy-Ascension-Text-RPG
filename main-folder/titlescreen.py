53c226dc3d068a5da72fb2cf963cc7687e4c3e56
SnakeOfFlowingTime <snakeofflowingtime@gmail.com>
2026-03-14 03:15:54 -0300

    changes to title screen (forgot what)

--- main-folder/titlescreen.py
+++ main-folder/titlescreen.py
@@ -4,7 +4,8 @@ def title_screen_options():
     import sys
     option = input('>').lower()
     if option == 'play':
-        start_game()
+        if start_game() == 'load':
+            return 'load'
     if option == 'help':
         help_menu()
     if option == 'quit':
@@ -13,7 +14,8 @@ def title_screen_options():
         print('please enter a valid option')
         option = input('>').lower()
         if option == 'play':
-            start_game()
+            if start_game() == 'load':
+                return 'load'
         if option == 'help':
             help_menu()
         if option == 'quit':
@@ -27,7 +29,8 @@ def title_screen():
     print("Type 'play' to start the game")
     print("Type 'help' to open the help menu")
     print("Type 'quit' to close the game")
-    title_screen_options()
+    if title_screen_options() == 'load':
+        return 'load'
 
 # Help menu
 def help_menu():
@@ -41,17 +44,23 @@ def start_game():
     print('do you wish to [Start a New Game (new)]')
     print('or do you wish to [Load Game (load)]')
     answer = input('>').lower()
-    if answer ==  'new':
+    if answer == 'load':
+        return 'load'
+    elif answer == 'new':
         print('are you sure, doing this will wipe your save file? Y/N')
         yes_no = input('>').lower()
         if yes_no == 'y':
             print(zones.zones['town square'].name + '\n' + zones.zones['town square'].description)
+            return 'y'       
         elif yes_no == 'n':
             title_screen()
+            return 'n'       
         else:
             print('invalid command')
             input('>')
             title_screen()
-    if answer == 'load':
-        print(zones.zones['town square'].name + '\n' + zones.zones['town square'].description)
-        return 'load'
+            return 'invalid command'
+    else:
+        print('invalid command')
+        title_screen()
+        return 'invalid command'
